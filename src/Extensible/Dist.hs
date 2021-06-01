fa66a81253aee49e411da2b55ddddd7bb23a246d
min-nguyen <minhnguyen1995@googlemail.com>
2021-06-01 15:54:21 +0100

    almost there with runDist (replacing Dist with samples)

--- src/Extensible/Dist.hs
+++ src/Extensible/Dist.hs
@@ -1,14 +1,18 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GADTs, TypeOperators #-}
 
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Extensible.Dist where
 
-import FreeT
+import Extensible.IO
+import Extensible.Freer
 import Sample
 import Util
 import Control.Lens hiding ((:>))
 import Control.Monad.State
 import Data.Maybe
-import Data.Extensible hiding (wrap, Head)
+import Data.Extensible hiding (wrap, Head, Member)
 import qualified Data.Vector as V
 import Statistics.Distribution
 import Statistics.Distribution.DiscreteUniform
@@ -39,3 +43,47 @@ data Dist a where
   CategoricalDist   :: V.Vector (Int, Double) -> Maybe Int -> Dist Int
   -- discrete       :: Dist Int
   DiscreteDist      :: [(Int, Double)] -> Maybe Int -> Dist Int
+
+sample :: Dist a -> Sampler a
+sample (NormalDist μ σ obs)  =
+  createSampler (sampleNormal μ σ) >>= return
+-- sample (UniformDist min max obs k )  = 
+--   createSampler (sampleUniform min max) >>= return . k
+-- sample (DiscrUniformDist min max obs k)  = 
+--   createSampler (sampleDiscreteUniform min max) >>= return . k
+-- sample (GammaDist k' θ obs k)        = 
+--   createSampler (sampleGamma k' θ) >>= return . k
+-- sample (BetaDist α β  obs k)         = 
+--   createSampler (sampleBeta α β) >>= return . k
+-- sample (BinomialDist n p  obs k)     = 
+--   createSampler (sampleBinomial n p) >>=  return . k . length
+-- sample (BernoulliDist p obs k)      = 
+--   createSampler (sampleBernoulli p) >>= return . k
+-- sample (CategoricalDist ps obs k)   = 
+--   createSampler (sampleCategorical (fmap snd ps)) >>= return . k
+-- sample (DiscreteDist ps obs k)      = 
+--   createSampler (sampleDiscrete (map snd ps)) >>= return . k
+
+-- runDist :: Member Dist rs => Freer (Dist ': rs) a -> Freer (Sampler ': rs) a
+-- runDist (Pure x) = return x
+-- runDist (Free u q) = case prj u of
+--      Just (NormalDist m sigma y) -> loop (lift (sample $ NormalDist m sigma y))
+
+-- runDist :: forall rs a. Freer (Dist  ': rs) a -> Freer rs a
+runDist :: Freer (Dist : rs) a 
+        -> Freer (Lift Sampler ': rs)  a
+runDist  m = loop m where
+  loop :: Freer (Dist : rs) a -> Freer (Lift Sampler ': rs) a
+  -- At this point, all Reader requests have been handled
+  loop (Pure x) = return x
+  -- Handle if Reader request, else ignore and go through the rest of the tree (by leaving the request's continuation k there to handle it, but also composing this with 'loop' so that the reader handler can then carry on afterwards).
+  loop (Free u k) = case decomp u of 
+    Right d@(NormalDist m sigma y) 
+      -> (send $ Lift (sample d) :: Freer (Lift Sampler ': rs) Double) >>= 
+        \x -> loop (k x) -- (send $ Lift (sample d)) >>= k
+    -- Left  u'  -> Free u' (loop . k)
+
+g :: Member (Lift Sampler) rs => Freer rs ()
+g = do 
+  send $ Lift (sample (NormalDist 0 0 Nothing))
+  return ()
--- src/Extensible/Inference.hs
+++ src/Extensible/Inference.hs
@@ -5,13 +5,25 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE PolyKinds #-}
  
+{-# LANGUAGE TypeOperators #-}
 module Extensible.Inference where
 
 import Data.Extensible
 import Control.Monad.Reader
 import Control.Monad.Trans.Class
-import Dist
-import Model hiding (runModel, runModelFree)
-import FreeT
+import Extensible.Dist
+import Extensible.Freer
+import Extensible.Model hiding (runModel, runModelFree)
 import Sample
-import Example
+
+-- runModelFree :: Model s rs a 
+--          -> Freer rs a
+-- runModelFree model = do
+--   let loop v = do
+--         x <- runFreeT v
+--         case x of 
+--           Free dist -> do
+--             a  <- lift $ sample dist 
+--             loop a 
+--           Pure v -> return v
+--   loop model 
