aaf703fcf0e956c340d147e62ff4fa9d31f6038f
min-nguyen <minhnguyen1995@googlemail.com>
2021-06-19 17:39:21 +0100

    added ModelF

--- src/ModelF.hs
+++ src/ModelF.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE RankNTypes, GADTs, TypeApplications, FlexibleInstances, DerivingStrategies, DataKinds, TypeOperators, TypeFamilies, FlexibleContexts, MultiParamTypeClasses, ConstraintKinds, PolyKinds, UndecidableSuperClasses, TemplateHaskell, ScopedTypeVariables, AllowAmbiguousTypes, QuantifiedConstraints, OverloadedLabels, UndecidableInstances, FunctionalDependencies, TypeFamilyDependencies #-}
+{-# LANGUAGE DeriveFunctor #-}
 
-module Model where
+module ModelF where
 
-import Dist
-import FreeT
-import Sampler
+
+-- import FreeT
+import Sample
 import Util
 import GHC.Generics
 import GHC.Types
@@ -18,14 +19,42 @@ import Control.Monad.Trans.Class ( MonadTrans(lift) )
 import Control.Monad.Reader
 import Control.Monad.Trans.Identity
 
+data DistF a x 
+  = Pure a 
+  | NormalF Double Double (Maybe Double) (Double -> x)
+  | BernoulliF Double (Maybe Bool) (Bool -> x)
+  | BinomialF Int Double (Maybe Int) (Int -> x)
 
-mkField "mu sigma y ys label"
+newtype DistT m a = DistT { runDistT :: m (DistF a (DistT m a)) }
 
-type ModelT s t a =
-  (MonadTrans t, Monad (t Sampler)) =>
-  FreeT Dist (ReaderT (Record (Maybes s)) (t Sampler)) a
+suspend :: Monad m => DistF a (DistT m a) -> DistT m a
+suspend fx = DistT (return fx)
 
-type Model s a = ModelT s IdentityT a
+instance Functor (DistF a) where
+  fmap g (NormalF mu sigma y fx) = NormalF mu sigma y (fmap g fx)
+  fmap g (BernoulliF p y fx) = BernoulliF p y (fmap g fx)
+  fmap g (BinomialF n p y fx) = BinomialF n p y (fmap g fx)
+  fmap g (Pure x)  = Pure x
+
+instance (Monad m) => Functor (DistT m) where
+    fmap = liftM
+
+instance (Monad m) => Applicative (DistT m) where
+    pure  = return
+    (<*>) = ap
+
+instance (Monad m) => Monad (DistT m) where
+    return  = DistT . return . Pure
+    m >>= f = DistT $ do
+        x <- runDistT m
+        runDistT $ case x of
+            Pure a -> f a
+            NormalF mu sigma y k -> DistT $ return $ NormalF mu sigma y (k >=> f)
+            BernoulliF p y k -> DistT $ return $ BernoulliF p y (k >=> f)
+            BinomialF n p y k -> DistT $ return $ BinomialF n p y (k >=> f)
+
+instance MonadTrans DistT where
+    lift = DistT . fmap Pure
 
 type family Keys (as :: [Assoc k v]) :: [k] where
   Keys ((k :> v) : as) = k : Keys as
@@ -56,12 +85,6 @@ instance Lookup (Maybes xs) k (Maybe v) => HasVar xs k v where
 
 type MRec s = Record (Maybes s)
 
-type X =
-    '[  "mu"    ':>  Double
-      , "sigma" ':>  Double
-      , "y"     ':>  Double
-     ]
-
 class AllNothing (a :: [Assoc Symbol *]) where
   allNothing :: MRec a
 
@@ -71,76 +94,49 @@ instance AllNothing '[] where
 instance AllNothing xs => AllNothing ((x ':> y) ': xs) where
   allNothing = xlb (Proxy @x) @= Nothing <: allNothing
 
-nothingRecord = allNothing :: MRec X
-
-g :: Wrapper h => FieldName k -> Repr h v -> Field h (k ':> v)
-g k v = k @= v
-
-h :: Field h kv -> h (TargetOf kv)
-h (Field i) = i
-
-me :: Wrapper h => FieldName k -> Repr h v -> Field h (k ':> v)
-me _ = Field #. review _Wrapper
-
-exampleParams :: MRec X
-exampleParams = mu @= Just 5 <: sigma @= Just 2 <: y @= Just 0 <: emptyRecord
-
 access :: Monad m
   => Getting a (s :& Field Identity) a
-  -> FreeT Dist (ReaderT (Record s) m) a
+  -> DistT (ReaderT (Record s) m) a
 access f = do
     env <- lift ask
     return (env ^. f)
 
 {- Distribution functions -}
 normal :: (a ~ Maybe Double, Monad m)
-  => Double -> Double -> Maybe Double
-  -> FreeT Dist (ReaderT (Record s) m) Double
+  => Double -> Double -> Maybe Double 
+  -> DistT (ReaderT (Record s) m) Double
 normal mu sigma maybe_y = do
-  suspend (NormalDist mu sigma maybe_y return)
+  suspend (NormalF mu sigma maybe_y return)
 
 normal' :: (a ~ Maybe Double, Monad m)
   => Double -> Double -> Getting a (s :& Field Identity) a
-  -> FreeT Dist (ReaderT (Record s) m) Double
+  -> DistT (ReaderT (Record s) m) Double
 normal' mu sigma field = do
   y <- access field
-  suspend (NormalDist mu sigma y return)
+  suspend (NormalF mu sigma y return)
 
 bernoulli :: (a ~ Maybe Bool, Monad m)
   => Double -> Maybe Bool
-  -> FreeT Dist (ReaderT (Record s) m) Bool
+  -> DistT (ReaderT (Record s) m) Bool
 bernoulli p maybe_y = do
-  suspend (BernoulliDist p maybe_y return)
+  suspend (BernoulliF p maybe_y return)
 
 bernoulli' :: (a ~ Maybe Bool, Monad m)
   => Double -> Getting a (s :& Field Identity) a
-  -> FreeT Dist (ReaderT (Record s) m) Bool
+  -> DistT (ReaderT (Record s) m) Bool
 bernoulli' p field = do
   y <- access field
-  suspend (BernoulliDist p y return)
+  suspend (BernoulliF p y return)
 
 binomial :: (a ~ Maybe Int, Monad m)
   => Int -> Double -> Maybe Int
-  -> FreeT Dist (ReaderT (Record s) m) Int
+  -> DistT (ReaderT (Record s) m) Int
 binomial n p maybe_y = do
-  suspend (BinomialDist n p maybe_y return)
+  suspend (BinomialF n p maybe_y return)
 
 binomial' :: (a ~ Maybe Int, Monad m)
   => Int -> Double -> Getting a (s :& Field Identity) a
-  -> FreeT Dist (ReaderT (Record s) m) Int
+  -> DistT (ReaderT (Record s) m) Int
 binomial' n p field = do
   y <- access field
-  suspend (BinomialDist n p y return)
-
--- FreeT Dist (ReaderT (Record (Maybes s)) m) a
-{- Executing Models -}
-runModelFree :: (MonadTrans t, Monad (t Sampler)) => ModelT s t a -> ReaderT (MRec s) (t Sampler) a
-runModelFree model = do
-  let loop v = do
-          x <- runFreeT v
-          case x of FreeF (NormalDist mu sigma y f) -> loop (f 5)
-                    Pure v -> return v
-  loop model
-
-runModel :: (MonadTrans t, Monad (t Sampler)) => ModelT s t a -> MRec s -> t (Sampler) a
-runModel model = runReaderT (runModelFree model)
+  suspend (BinomialF n p y return)
