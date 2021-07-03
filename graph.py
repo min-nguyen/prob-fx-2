3080630e57bd45f6e02711fe81e4c97c5fea9c9a
min-nguyen <minhnguyen1995@googlemail.com>
2021-07-03 18:06:48 +0100

    added curve fit for log-regr-basic

--- graph.py
+++ graph.py
@@ -4,19 +4,47 @@ import matplotlib.pyplot as plt
 import ast
 import pandas as pd
 from sklearn import linear_model
+from sklearn import datasets
+from scipy.special import expit
+import numpy as np
 
 def main():
   arg  = sys.argv[1]
   f    = open("model-output.txt", "r")
   data = ast.literal_eval(f.read())
+  if arg == "log-regr-test":
+    iris = datasets.load_iris()
+    # Get first 100 x data points. iris.data consists of a list of 4-dimensional data points. We will only use the 1st dimension for x data points.
+    iris_data = (iris.data[0:100])[:,0]
+    # Get first 100 y data points
+    iris_target = iris.target[0:100]
+    # Create model for logistic regression
+    model = linear_model.LogisticRegression(C=1e5, solver='lbfgs')
+    # Make xs into a list of lists and fit the data
+    model.fit(iris_data.reshape(-1,1), iris_target)
+    # predict dummy y_test data based on the logistic model
+    x_test = np.linspace(4.0,7.0,num=100)
+    y_test = x_test * model.coef_ + model.intercept_
+    # take the sigmoid of y_test
+    sigmoid = expit(y_test)
+    plt.scatter(iris_data, iris_target)
+    plt.plot(x_test,sigmoid.ravel(),c="green", label = "logistic fit")
+    plt.xlabel("Sepal Length")
+    plt.ylabel("Sepal Width")
+    plt.show()
   if arg == "log-regr-basic":
     xys =  [[ i for i, j in data ],
             [ j for i, j in data ]]
-    print(xys)
-    xs = xys[0]
-    ys = xys[1]
-
+    # # print(xys)
+    xs = np.array([ [x] for x in xys[0] ])
+    ys = np.array([ y for y in xys[1] ])
+    model = linear_model.LogisticRegression(C=1e5, solver='lbfgs')
+    model.fit(xs.reshape(-1,1), ys)
+    x_test = np.linspace(-2.0,2.0,num=100)
+    y_test = x_test * model.coef_ + model.intercept_
+    sigmoid = expit(y_test)
     plt.scatter(xs, ys)
+    plt.plot(x_test, sigmoid.ravel(),c="green", label = "logistic fit")
     plt.xlabel('x - axis')
     plt.ylabel('y - axis')
     plt.title('Logistic regression')
