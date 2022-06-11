af3a328011e373163d408e28d2db5dc92d7954b9
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-06-11 14:18:05 +0100

    added example of compiling to MBayes

---
+++ src/Examples/MBayes/LinRegr.hs
@@ -0,0 +1,71 @@
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant return" #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+module Examples.LinRegr where
+
+import Effects.ObsReader
+import Effects.Writer
+import Model
+import Inference.MBAYES
+import Effects.Dist
+import Effects.Lift
+import Control.Monad
+import Env
+import Util
+import Control.Monad.Bayes.Class (MonadInfer)
+import Control.Monad.Bayes.Weighted ( prior, runWeighted )
+import Control.Monad.Bayes.Sampler
+import Control.Monad.Bayes.Traced
+import Numeric.Log
+
+type LinRegrEnv =
+  '[ "m" ':= Double,
+     "c" ':= Double,
+     "σ" ':= Double,
+     "y" ':= Double
+   ]
+
+{-  Linear regression  -}
+linRegr :: forall env rs .
+  Observables env '["y", "m", "c", "σ"] Double =>
+  [Double] -> Model env rs [Double]
+linRegr xs = do
+  m <- normal 0 3 #m
+  c <- normal 0 5 #c
+  σ <- uniform 1 3 #σ
+  ys <- mapM (\x -> do
+                    y <- normal (m * x + c) σ #y
+                    return y) xs
+  return ys
+
+mbayesLinRegr :: (MonadInfer m, Observables env '["y", "m", "c", "σ"] Double) =>
+ [Double] -> Env env -> m [Double]
+mbayesLinRegr xs = toMBayes (linRegr xs)
+
+simLinRegr :: Int -> Int -> IO [[Double]]
+simLinRegr n_samples n_datapoints = do
+  let xs  = [0 .. fromIntegral n_datapoints]
+      env = (#m := [3.0]) <:> (#c := [0]) <:> (#σ := [1]) <:> (#y := []) <:> eNil
+  sampleIO $ prior $ replicateM n_samples (mbayesLinRegr xs env)
+
+lwLinRegr :: Int -> Int -> IO [([Double], Log Double)]
+lwLinRegr n_datapoints n_samples = do
+  let n_datapoints' = fromIntegral n_datapoints
+      xs            = [0 .. n_datapoints']
+      env           = (#y := [3*x | x <- xs]) <:> (#m := []) <:> (#c := []) <:> (#σ := []) <:>  eNil
+  sampleIO $ replicateM n_samples (runWeighted $ mbayesLinRegr xs env)
+
+mhLinRegr :: Int -> Int -> IO [[Double]]
+mhLinRegr n_samples n_datapoints = do
+  let n_datapoints' = fromIntegral n_datapoints
+      xs            = [0 .. n_datapoints']
+      env           = (#y := [3*x | x <- xs]) <:> (#m := []) <:> (#c := []) <:> (#σ := []) <:>  eNil
+  sampleIO $ prior $ mh n_samples (mbayesLinRegr xs env)
--- src/Inference/MBAYES.hs
+++ src/Inference/MBAYES.hs
@@ -7,33 +7,27 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <&>" #-}
 
-module Inference.MBayes where
+module Inference.MBAYES where
 
 import Prog
-import Effects.Dist hiding (handleDist)
+import Model (Model(..))
+import Effects.Dist
 import Effects.Lift
+import Effects.ObsReader
+import Env
 import Control.Monad.Bayes.Class
 import Control.Monad
 import Numeric.Log
 import Control.Monad.IO.Class
 import qualified Data.Vector as Vec
 
-{-  Handle Dist as last effect directly into a monad -}
-
-handleDist :: forall m es a. MonadInfer m => Prog '[Dist] a -> m a
-handleDist (Val x)  = return x
-handleDist (Op u k) = case discharge u of
-    Right d ->
-      case getObs d of
-          Just y  -> do let p = logProb d y
-                        score (Exp p)
-                        handleDist (k y)
-          Nothing -> do y <- sampleBayes d
-                        handleDist (k y)
-    Left  u'  -> error "impossible; Dist must be the last effect"
-
 {- Handle Obs and Sample separately, using the "Lift m" effect and a MTL approach to "m" -}
+
+toMBayes :: forall m env a. MonadInfer m => Model env [ObsReader env, Dist, Lift m] a -> Env env -> m a 
+toMBayes m env = (handleLift . handleSamp @m . handleObs @m . handleDist . handleObsRead env) (runModel m)
 
 handleObs :: forall m es a. MonadCond m => Member (Lift m) es => Prog (Observe : es) a -> Prog es a
 handleObs (Val x)  = Val x
@@ -45,7 +39,7 @@ handleObs (Op u k) = case discharge u of
          lift @m $ score (Exp p)
          handleObs @m (k y)
 
-handleSamp :: forall m es a. (MonadIO m, MonadSample m) => Member (Lift m) es => Prog (Sample : es) a -> Prog es a
+handleSamp :: forall m es a. MonadSample m => Member (Lift m) es => Prog (Sample : es) a -> Prog es a
 handleSamp (Val x) = return x
 handleSamp (Op u k) = case discharge u of
   Left u' -> do
@@ -53,9 +47,8 @@ handleSamp (Op u k) = case discharge u of
   Right (Sample d _) -> 
       do y <- lift @m $ sampleBayes d
          handleSamp @m (k y)
-  Right (Printer s) ->
-      do lift @m (liftIO $ print "")
-         handleSamp @m (k ())
+  Right (Printer s) ->  -- Ignoring printing for now so the `MonadIO m` constraint can be omitted.
+      do handleSamp @m (k ())
 
 sampleBayes :: MonadSample m => Dist a -> m a
 sampleBayes (UniformDist a b _ _)     = uniform a b
@@ -69,3 +62,18 @@ sampleBayes (BinomialDist n p _ _)    = replicateM n (bernoulli p) >>= (pure . l
 sampleBayes (PoissonDist l _ _)       = poisson l
 sampleBayes (DirichletDist as _ _)    = dirichlet (Vec.fromList as) >>= pure . Vec.toList
 sampleBayes (DistDict d)              = error ("Sampling from " ++ show d ++ " is not supported")
+
+
+{-  Alternative for handling Dist as the last effect directly into a monad -}
+
+handleDist_MB :: forall m es a. MonadInfer m => Prog '[Dist] a -> m a
+handleDist_MB (Val x)  = return x
+handleDist_MB (Op u k) = case discharge u of
+    Right d ->
+      case getObs d of
+          Just y  -> do let p = logProb d y
+                        score (Exp p)
+                        handleDist_MB (k y)
+          Nothing -> do y <- sampleBayes d
+                        handleDist_MB (k y)
+    Left  u'  -> error "impossible; Dist must be the last effect"
