c9da1e0bcf7a08ae9fb0ee271d04a1647776d567
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-06-19 12:00:07 +0100

    exploring why my version doesn't work in Fused/ExampleSrc.hs

---
+++ src/Fused/SrcComparison/AlgebraSrc.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+
+module Fused.SrcComparison.AlgebraSrc where
+
+import Fused.Sum
+import Data.Functor.Identity
+
+
+type Handler ctx n m = forall x. ctx (n x) -> m (ctx x)
+
+class Monad m => Algebra sig m | m -> sig where
+  alg
+    :: Functor ctx
+    => Handler ctx n m -- ^ A 'Handler' lowering computations inside the effect into the carrier type @m@.
+    -> sig n a         -- ^ The effect signature to be interpreted.
+    -> ctx ()          -- ^ The initial state.
+    -> m (ctx a)       -- ^ The interpretation of the effect in @m@.
+
+type Has eff sig m = (Members eff sig, Algebra sig m)
+
+-- | Construct a request for an effect to be interpreted by some handler later on.
+send :: (Member eff sig, Algebra sig m) => eff m a -> m a
+send sig = runIdentity <$> alg (fmap Identity . runIdentity) (inj sig) (Identity ())
+
+run = runIdentity 
+
+-- | Compose two contexts, and compose two handlers
+newtype Compose f g a = Compose { getCompose :: f (g a) } deriving Functor
+
+infixr 1 ~<~
+(~<~) :: (Functor n, Functor ctx1) => Handler ctx1 m n -> Handler ctx2 l m -> Handler (Compose ctx1 ctx2) l n
+hdl1 ~<~ hdl2 = fmap Compose . hdl1 . fmap hdl2 . getCompose
+
+-- | Thread a composed handler and input state through the algebra for some underlying signature.
+thread :: (Functor ctx1, Functor ctx2, Algebra sig m)
+  => Handler (Compose ctx1 ctx2) n m
+  -> sig n a
+  -> ctx1 (ctx2 ())
+  -> m (ctx1 (ctx2 a))
+thread hdl op = fmap getCompose . alg hdl op . Compose
+
+newtype Swap s a = Swap { getSwap :: (a, s) }
+  deriving Functor
---
+++ src/Fused/SrcComparison/ExampleSrc.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FunctionalDependencies, FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators, TypeApplications, UndecidableInstances #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use void" #-}
+{-# LANGUAGE TupleSections #-}
+
+module Fused.ExampleSrc where
+
+import qualified Control.Algebra 
+import qualified Control.Carrier.Reader
+import qualified Control.Carrier.State.Lazy ()
+
+import Fused.SrcComparison.AlgebraSrc ( Algebra(..), Has, send, run )
+import Fused.SrcComparison.ReaderSrc
+import Fused.Sum
+import Fused.Writer
+import Fused.Lift
+
+exampleSrc :: String
+exampleSrc = Control.Algebra.run . Control.Carrier.Reader.runReader "hello" $ Control.Carrier.Reader.ask
+
+example :: String
+example = run $ runReader "hello" $ ask
