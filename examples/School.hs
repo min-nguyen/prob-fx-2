b726343702e50e6c2f981f1404d242f91ef6328c
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-17 17:34:51 +0100

    [Documentation] School

--- examples/School.hs
+++ examples/School.hs
@@ -6,24 +6,34 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedLabels #-}
 
+{- | The Gelman and Hill [8-schools case study](https://cran.r-project.org/web/packages/rstan/vignettes/rstan.html),
+     which quantifies the effect of coaching programs from 8 different schools on students' SAT-V scores.
+-}
+
 module School where
 
-import Model
-import Inference.MH as MH
-import Sampler
-import Control.Monad
+import Model ( Model, deterministic, normal, normal', halfNormal' )
+import Inference.MH as MH ( mh )
+import Sampler ( Sampler )
+import Control.Monad ( replicateM )
 import Data.Kind (Constraint)
-import Env
+import Env ( Env(..), Observables, Observable, Assign ((:=)), (<:>), enil, (<#>), vnil, get)
 
--- ||| Hierarchical School Model
+-- | School model environment
 type SchEnv = '[
-    "mu"    ':= Double,
-    "theta" ':= [Double],
-    "y"     ':= Double
+    "mu"    ':= Double,   -- ^ effect of general coaching programs on SAT scores
+    "theta" ':= [Double], -- ^ variation of each program's effect on SAT scores
+    "y"     ':= Double    -- ^ effectiveness on SAT scores
   ]
 
+-- | School model
 schoolModel :: (Observables env '["mu", "y"] Double, Observable env "theta" [Double])
-  => Int -> [Double] -> Model env es [Double]
+  -- | number of schools
+  => Int
+  -- | standard errors of each school
+  -> [Double]
+  -- | effectiveness of each school
+  -> Model env es [Double]
 schoolModel n_schools σs = do
   μ   <- normal 0 10 #mu
   τ   <- halfNormal' 10
@@ -32,13 +42,18 @@ schoolModel n_schools σs = do
   ys  <- mapM (\(θ, σ) -> normal θ σ #y) (zip θs σs)
   return θs
 
-mhSchool :: Int -> Sampler ([Double], [[Double]])
+-- | Perform MH inference
+mhSchool :: Int ->  Sampler ([Double], [[Double]])
 mhSchool n_mhsteps = do
+  -- Specify model inputs
   let n_schools = 8
       ys        = [28, 8, -3,   7, -1,  1, 18, 12]
       sigmas    = [15, 10, 16, 11,  9, 11, 10, 18]
-      env       = #mu := [] <:> #theta := [] <:> #y := ys <:> enil
+  -- Specify model environment
+      env       = #mu := [] <:> #theta := [] <:> #y := ys <:> ENil
+  -- Run MH inference for 10000 iterations
   env_mh_out <- MH.mh n_mhsteps (schoolModel n_schools sigmas) env (#mu <#> #theta <#> vnil)
+  -- Retrieve and returns the trace of model parameters mu and theta
   let mus    = concatMap (get #mu) env_mh_out
       thetas = concatMap (get #theta) env_mh_out
   return (mus, thetas)
