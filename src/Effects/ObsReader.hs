8f17d8da4ebf1526095efca2c2f7e9b9673910db
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-19 14:29:35 +0100

    [Documentation] Effects.ObsReader

--- src/Effects/ObsReader.hs
+++ src/Effects/ObsReader.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators, TypeApplications #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 
-{- | An effect for reading observable variables from a model environment -}
+{- | The effect for reading observable variables from a model environment.
+-}
 
 module Effects.ObsReader (
     ObsReader(..)
@@ -18,20 +20,22 @@ import Util ( safeHead, safeTail )
 
 -- | The effect for reading observed values from a model environment @env@
 data ObsReader env a where
-  -- | Given variable @x@ is assigned a list of type @[a]@ in @env@, attempt to retrieve its head value
-  Ask :: Observable env x a 
-    => Var x                   -- ^ Variable @x@ to read from
-    -> ObsReader env (Maybe a) -- ^ The head value from @x@'s list
+  -- | Given the observable variable @x@ is assigned a list of type @[a]@ in @env@, attempt to retrieve its head value.
+  Ask :: Observable env x a
+      => Var x                    -- ^ variable @x@ to read from
+      -> ObsReader env (Maybe a)  -- ^ the head value from @x@'s list
 
--- | Retrieve the first value from the trace of an observable variable if it exists
-ask :: forall env es x a. Member (ObsReader env) es => Observable env x a => Var x -> Prog es (Maybe a)
+-- | Wrapper function for calling @Ask@
+ask :: forall env es x a. (Member (ObsReader env) es, Observable env x a)
+  => Var x
+  -> Prog es (Maybe a)
 ask x = call (Ask @env x)
 
 -- | Handle the @Ask@ requests of observable variables
 handleObsRead ::
   -- | Initial model environment
-     Env env 
-  -> Prog (ObsReader env ': es) a 
+     Env env
+  -> Prog (ObsReader env ': es) a
   -> Prog es a
 handleObsRead env (Val x) = return x
 handleObsRead env (Op op k) = case discharge op of
