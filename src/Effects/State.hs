525b89e9e19a066d976eeced43269ebb1105d4a2
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-19 14:31:51 +0100

    [Documentation] Effects.State

--- src/Effects/State.hs
+++ src/Effects/State.hs
@@ -4,13 +4,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
-
-{- | The state effect
+{- | State effect.
 -}
 
 module Effects.State (
     State(..)
-  , get 
+  , get
   , getM
   , put
   , putM
@@ -19,40 +18,41 @@ module Effects.State (
   , handleStateM) where
 
 import Prog ( Member(inj), Prog(..), discharge )
-import Model ( Model(..) ) 
+import Model ( Model(..) )
 
 -- | The state effect
 data State s a where
+  -- | Get the current state
   Get :: State s s
+  -- | Set the current state
   Put :: s -> State s ()
 
--- | Get the state
+-- | Wrapper function for @Get@
 get :: (Member (State s) es) => Prog es s
 get = Op (inj Get) Val
 
--- | Get the state in a model
+-- | Wrapper function for @Get@ in a model
 getM :: (Member (State s) es) => Model env es s
 getM = Model get
 
--- | Set the state
+-- | Wrapper function for @Set@
 put :: (Member (State s) es) => s -> Prog es ()
 put s = Op (inj $ Put s) Val
 
--- | Set the state in a model
+-- | Wrapper function for @Set@ in a model
 putM :: (Member (State s) es) => s -> Model env es ()
 putM s = Model (put s)
 
--- | Apply a function to the state
+-- | Wrapper function for apply a function to the state
 modify :: Member (State s) es => (s -> s) -> Prog es ()
 modify f = get >>= put . f
 
 -- | Handle the @State s@ effect
-handleState :: 
-  -- | Initial state
-     s 
-  -- | Initial program
-  -> Prog (State s ': es) a 
-  -- | Pure value and final state
+handleState ::
+  -- | initial state
+     s
+  -> Prog (State s ': es) a
+  -- | (output, final state)
   -> Prog es (a, s)
 handleState s m = loop s m where
   loop :: s -> Prog (State s ': es) a -> Prog es (a, s)
