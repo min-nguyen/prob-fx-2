bc213b080bf6ee7bf392a8404fa4ecd670eaa7cb
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-23 12:29:25 +0100

    [Added] Effects.NonDet

---
+++ src/Effects/NonDet.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+
+{- | Non-determinism effect.
+-}
+
+module Effects.NonDet (
+    NonDet(..)
+  , asum
+  , branch
+  , branchWeaken
+  , weakenNonDet
+  , accumNonDet
+  , handleNonDet
+  ) where
+
+import Control.Applicative ( Alternative((<|>), empty) )
+import Prog ( call, discharge, weaken, EffectSum, Member, Prog(..) )
+
+-- |  The effect for non-deterministic computations
+data NonDet a where
+  -- | The operation @Empty@ represents there being no computation branches.
+  Empty  :: NonDet a
+  -- | The operation @Choose@ is parameterised by a boolean flag, indicating whether to take the left or right computation branch.
+  Choose :: NonDet Bool
+
+instance Member NonDet es => Alternative (Prog es) where
+  empty  = call Empty
+  m1 <|> m2 = do b <- call Choose
+                 if b then m1 else m2
+
+-- | Fold a collection of programs into a non-deterministic computation
+asum :: (Foldable t, Member NonDet es) => t (Prog es a) -> Prog es a
+asum = foldr (<|>) (call Empty)
+
+-- | Branch a program into @n@ computations
+branch :: Member NonDet es => Int -> Prog es a -> Prog es a
+branch n prog = asum (replicate n prog)
+
+-- | Install the non-determinism effect to a program and then branch into @n@ computations
+branchWeaken :: Int -> Prog es a -> Prog (NonDet : es) a
+branchWeaken n (Val x)   = asum $ replicate n (Val x)
+branchWeaken n (Op op k) = asum $ replicate n (Op (weaken op) (weaken' . k))
+  where weaken' (Op op k) = Op (weaken op) (weaken' . k)
+        weaken' (Val x)   = Val x
+
+-- | Install the non-determinism effect to a program
+weakenNonDet :: Prog es a -> Prog (NonDet : es) a
+weakenNonDet = branchWeaken 1
+
+{- | Check whether a list of programs have all terminated.
+     If at least one program is unfinished, return all programs.
+     If all programs have finished, return a single program that returns all results.
+-}
+accumNonDet :: [Prog es' a] -> Either [Prog es' a] (Prog es [a])
+accumNonDet (Val v : progs) = do
+  vs <- accumNonDet progs
+  pure ((v:) <$> vs)
+accumNonDet [] = pure (Val [])
+accumNonDet progs = Left progs
+
+-- | Handle the @NonDet@ effect by running all computation branches
+handleNonDet :: Prog (NonDet ': es) a -> Prog es [a]
+handleNonDet (Val x) = pure [x]
+handleNonDet (Op op k) = case discharge op of
+   Right Choose -> (<|>) <$> handleNonDet (k True) <*> handleNonDet (k False)
+   Right Empty  -> Val []
+   Left op  -> Op op (handleNonDet . k)
