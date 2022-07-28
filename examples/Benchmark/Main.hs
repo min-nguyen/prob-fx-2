4471c343902082bcd2c2c685885d8c33a727f22a
Minh Nguyen <minhnguyen1995@googlemail.com>
2022-07-28 12:37:38 +0100

    [Added] lw examples for LDA and HMM, for benchmarking purposes + [Added] Benchmark.Main

---
+++ examples/Benchmark/Main.hs
@@ -0,0 +1,115 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# LANGUAGE RankNTypes #-}
+{-# HLINT ignore "Use camelCase" #-}
+module Main where
+
+import LinRegr
+import HMM
+import LDA
+
+import Criterion.Main
+import Criterion.Types
+import Data.List
+import Sampler
+import Data.List.Split
+import Statistics.Types
+import Control.DeepSeq
+import Criterion (benchmark')
+
+{- | Benchmarking utility
+-}
+fixed_fileName :: String
+fixed_fileName = "benchmarks.csv"
+
+appendFileLn :: String -> String -> IO ()
+appendFileLn file_name = appendFile file_name . (++ "\n")
+
+writeRow :: Show a => String -> (String, [a]) -> IO ()
+writeRow file_name (label, values) = appendFileLn file_name (intercalate "," (label : map show values))
+
+benchMean :: NFData a => IO a -> IO Double
+benchMean prog = do
+  report <- benchmark' (nfIO prog)
+  let analysis = reportAnalysis report
+      estMean  = estPoint (anMean analysis)
+  return estMean
+
+benchRow :: NFData a
+  -- | (Program name, program)
+  => (String, Int -> Sampler a)
+  -- | (Independent variable name, values)
+  -> (String, [Int])
+  -- | List of run-times
+  -> IO ()
+benchRow (prog_name, prog) (_, params) = do
+  -- Run program over varying parameter values and write e.g. "LinRegr-MH100, 0.23, 0.87, 1.23, 1.78, 2.45"
+  means <- mapM (benchMean . sampleIO . prog) params
+  writeRow fixed_fileName (prog_name, means)
+
+{- | Varying over dataset size
+-}
+fixed_lw_steps :: Int
+fixed_lw_steps = 2000
+fixed_mh_steps :: Int
+fixed_mh_steps = 2000
+
+bench_LR :: [Int] -> IO ()
+bench_LR args = do
+    let row_header = ("Dataset size", args)
+    writeRow fixed_fileName row_header
+    benchRow ("LR-SIM", simLinRegr) row_header
+    benchRow ("LR-LW2000", lwLinRegr fixed_lw_steps) row_header
+    benchRow ("LR-MH2000", mhLinRegr fixed_mh_steps) row_header
+
+bench_HMM :: [Int] -> IO ()
+bench_HMM args = do
+    let row_header = ("Dataset size", args)
+    writeRow fixed_fileName row_header
+    benchRow ("HMM-SIM", simHMM) row_header
+    benchRow ("HMM-LW2000", lwHMM fixed_lw_steps) row_header
+    benchRow ("HMM-MH2000", mhHMM fixed_mh_steps) row_header
+
+bench_LDA :: [Int] -> IO ()
+bench_LDA args = do
+    let row_header = ("Dataset size", args)
+    writeRow fixed_fileName row_header
+    benchRow ("LDA-SIM", simLDA) row_header
+    benchRow ("LDA-LW2000", lwLDA fixed_lw_steps) row_header
+    benchRow ("LDA-MH2000", mhPredLDA fixed_mh_steps) row_header
+
+{- | Varying over inference parameters
+-}
+fixed_lr_datasize :: Int
+fixed_lr_datasize = 50
+fixed_hmm_datasize :: Int
+fixed_hmm_datasize = 20
+fixed_lda_datasize :: Int
+fixed_lda_datasize = 50
+
+-- bench_MH :: [Int] -> IO ()
+-- bench_MH args = do
+--     let row_header = ("Number of steps", args)
+--     writeRow fixed_fileName row_header
+--     benchRow ("MH-LR50", flip mhLinRegr fixed_lr_datasize) row_header
+--     benchRow ("MH-HMM20", flip mhHMM fixed_hmm_datasize) row_header
+--     benchRow ("MH-LDA50", flip mhLDA fixed_lda_datasize) row_header
+
+main :: IO ()
+main = pure ()
+-- runBenchmarks :: IO ()
+-- runBenchmarks = do
+--   -- | Read input benchmark parameters
+--   content <- readFile "../benchmark_params.txt"
+--   let removeComments :: [String] -> [String]
+--       removeComments = filter (\case []     -> False
+--                                      (x:xs) -> x /= '#')
+--   let args :: [[Int]]
+--       args = map (map read . splitOn ",") (removeComments (lines content))
+--   -- | Run benchmark programs on their corresponding parameters
+--   case args of
+--         [lr, hmm, lda, mh, smc, rmsmc] -> do
+--           bench_LR lr
+--           bench_HMM hmm
+--           bench_LDA lda
+--           bench_MH mh
+--         _   -> error "bad input file"
