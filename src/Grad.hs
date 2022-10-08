4d10b71d8e48bd6bbf02f83648ff04cbbf4a34ac
min-nguyen <minhnguyen1995@googlemail.com>
2022-10-08 10:22:35 +0100

    [Add] gammaGradLogPdfRaw

--- src/Grad.hs
+++ src/Grad.hs
@@ -16,12 +16,12 @@ import Numeric.AD.Mode.Reverse
 import Numeric.AD.Internal.Reverse
 import Numeric.MathFunctions.Constants
 import Numeric.SpecFunctions (
-  incompleteBeta, invIncompleteBeta, logBeta, digamma, log1p)
+  incompleteBeta, invIncompleteBeta, logBeta, logGamma, digamma, log1p)
 
 {- Normal -}
 -- Log pdf using just Doubles
-normalLogPdfDouble :: [Double] -> Double
-normalLogPdfDouble [mean, variance, y] = (-ym * ym / (2 * variance)) - (log (m_sqrt_2_pi * sqrt variance))
+normalLogPdfRaw :: [Double] -> Double
+normalLogPdfRaw [mean, variance, y] = (-ym * ym / (2 * variance)) - (log (m_sqrt_2_pi * sqrt variance))
   where ym = y - mean
 
 -- Log pdf explicitly using Reverse s Double
@@ -38,21 +38,38 @@ normalLogPdf_example :: Double
 normalLogPdf_example = normalLogPdf [0, 1, 0]
 
 -- Gradient of log pdf directly
-gradNormalLogPdfRaw :: [Double] -> [Double]
-gradNormalLogPdfRaw [mean, variance, y] = [ym/variance, -1/(2 * variance) + ((ym/sqrt variance)**2)/(2*variance), -ym/variance ]
+normalGradLogPdfRaw :: [Double] -> [Double]
+normalGradLogPdfRaw [mean, variance, y] = [dm, dv, dx]
   where ym = y - mean
+        dm = ym/variance
+        dv = -1/(2 * variance) + ((ym/sqrt variance)**2)/(2*variance)
+        dx = -dm
 
 -- Gradient of log pdf using AD
-gradNormalLogPdf :: [Double] -> [Double]
-gradNormalLogPdf = grad normalLogPdf
+normalGradLogPdf :: [Double] -> [Double]
+normalGradLogPdf = grad normalLogPdf
 
-gradNormalLogPdf_example :: [Double]
-gradNormalLogPdf_example = grad normalLogPdf [0, 1, 0]
+normalGradLogPdf_example :: [Double]
+normalGradLogPdf_example = grad normalLogPdf [0, 1, 0]
+
+{- Gamma -}
+
+-- Log pdf using just Doubles
+gammaLogPdfRaw :: [Double] -> Double
+gammaLogPdfRaw [k, t, x] = (k - 1) * log x - (x/t) - logGamma k - (k * log t)
+
+-- Gradient of log pdf directly
+gammaGradLogPdfRaw :: [Double] -> [Double]
+gammaGradLogPdfRaw [k, t, x] = [dk, dt, dx]
+  where dk = log x - digamma k - log t
+        dt = x/(t**2) - k/t
+        dx = (k - 1)/x - 1/t
 
 {- Beta -}
-betaLogPdf :: (Ord a, Floating a, Mode a, Scalar a ~ Double) => [a] -> a
-betaLogPdf [a, b, y]
-    | a <= 0 || b <= 0 = auto m_NaN
-    | y <= 0 = auto m_neg_inf
-    | y >= 1 = auto m_neg_inf
-    | otherwise = (a-1)*log y + (b-1)*log1p (-y) - auto (logBeta (fromJust $ asKnownConstant a) (fromJust $ asKnownConstant a))
+
+betaLogPdfRaw :: [Double] -> Double
+betaLogPdfRaw [a, b, y]
+    | a <= 0 || b <= 0 = m_NaN
+    | y <= 0 = m_neg_inf
+    | y >= 1 = m_neg_inf
+    | otherwise = (a-1)*log y + (b-1)*log1p (-y) - logBeta a a
