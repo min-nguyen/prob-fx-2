849df84af8340eadc9d775fd16ee8c30e253fccf
min-nguyen <minhnguyen1995@googlemail.com>
2023-01-08 17:04:14 +0000

    Move Param into independent module

--- src/Effects/Dist.hs
+++ src/Effects/Dist.hs
@@ -18,23 +18,9 @@ module Effects.Dist (
   -- ** Dist effect
   , Dist(..)
   , handleDist
-  -- ** Sample effect
-  , Sample(..)
-  , pattern SampPrj
-  , pattern SampDis
-  -- ** Observe effect
-  , Observe(..)
-  , pattern ObsPrj
-  , pattern ObsDis
-  -- ** Param effect
-  , Param(..)
-  , pattern ParamPrj
-  , setParamDist
-  -- ** Param effect
-  , Score(..)
-  , pattern ScorePrj
   , module Effects.Sample
   , module Effects.Observe
+  , module Effects.Param
   ) where
 
 import           Data.Maybe ( fromMaybe )
@@ -45,6 +31,7 @@ import           Prog ( call, discharge, weaken, Member(..), Prog(..), EffectSum
 import           Util
 import           Effects.Sample
 import           Effects.Observe
+import           Effects.Param
 
 -- | The effect @Dist@ for primitive distributions
 data Dist a where
@@ -54,32 +41,6 @@ data Dist a where
            , getTag :: Maybe Tag      -- ^ optional observable variable name
            }
         -> Dist a
-
--- | The effect @Param@ for distributions with support for gradient log-pdfs
-data Param a where
-  Param :: (DiffDistribution d, a ~ Base d)
-         => d              -- ^ proposal distribution
-         -> Addr           -- ^ address of operation
-         -> Param a        -- ^ observed point
-
-setParamDist :: (DiffDistribution d, a ~ Base d) => Param a -> d -> Param a
-setParamDist (Param q α) q' = Param q' α
-
--- | For projecting and then successfully pattern matching against @Param@
-pattern ParamPrj :: (Member Param es) => (DiffDistribution d, x ~ Base d) => d -> Addr -> EffectSum es x
-pattern ParamPrj q α <- (prj -> Just (Param q α))
-
--- | The effect @Score@ is like @Param@ but retains the original distribution to be optimised
-data Score a where
-  Score  :: (DiffDistribution d, a ~ Base d)
-         => d              -- ^ original distribution
-         -> d              -- ^ proposal distribution
-         -> Addr           -- ^ address of operation
-         -> Score a        -- ^ observed point
-
--- | For projecting and then successfully pattern matching against @Score@
-pattern ScorePrj :: (Member Score es) => (DiffDistribution d, x ~ Base d) => d -> d -> Addr -> EffectSum es x
-pattern ScorePrj d q α <- (prj -> Just (Score d q α))
 
 -- | Handle the @Dist@ effect to a @Sample@ or @Observe@ effect and assign an address
 handleDist :: Prog (Dist : es) a -> Prog (Observe : Sample : es) a
---
+++ src/Effects/Param.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Effects.Param where
+
+import Prog
+import PrimDist
+import Trace
+
+-- | The effect @Param@ for distributions with support for gradient log-pdfs
+data Param a where
+  Param :: (DiffDistribution d, a ~ Base d)
+         => d              -- ^ proposal distribution
+         -> Addr           -- ^ address of operation
+         -> Param a        -- ^ observed point
+
+setParamDist :: (DiffDistribution d, a ~ Base d) => Param a -> d -> Param a
+setParamDist (Param q α) q' = Param q' α
+
+-- | For projecting and then successfully pattern matching against @Param@
+pattern ParamPrj :: (Member Param es) => (DiffDistribution d, x ~ Base d) => d -> Addr -> EffectSum es x
+pattern ParamPrj q α <- (prj -> Just (Param q α))
+
+-- | The effect @Score@ is like @Param@ but retains the original distribution to be optimised
+data Score a where
+  Score  :: (DiffDistribution d, a ~ Base d)
+         => d              -- ^ original distribution
+         -> d              -- ^ proposal distribution
+         -> Addr           -- ^ address of operation
+         -> Score a        -- ^ observed point
+
+-- | For projecting and then successfully pattern matching against @Score@
+pattern ScorePrj :: (Member Score es) => (DiffDistribution d, x ~ Base d) => d -> d -> Addr -> EffectSum es x
+pattern ScorePrj d q α <- (prj -> Just (Score d q α))
