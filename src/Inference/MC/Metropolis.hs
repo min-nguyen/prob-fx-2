dafca03639441b9d6cac3efc9ea8f771c59d89c0
min-nguyen <minhnguyen1995@googlemail.com>
2023-02-01 15:54:01 +0000

    rework metroStep to take and return a single proposal

--- src/Inference/MC/Metropolis.hs
+++ src/Inference/MC/Metropolis.hs
@@ -5,6 +5,8 @@
 
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <&>" #-}
 
 {- | Metropolis inference
 -}
@@ -47,36 +49,36 @@ type ModelHandler es p = forall a. Model es a -> Trace -> Sampler ((a, p), Trace
 
 {- | A general framework for Metropolis inference.
 -}
-metropolis :: (Members [Proposal p, Sampler] fs)
+metropolis :: forall p fs es a. (Members [Proposal p, Sampler] fs)
    => Int                                                                    -- ^ number of iterations
    -> Trace                                                          -- ^ initial context + sample trace
    -> ModelHandler es p                                                        -- ^ model handler
    -> Model es a                                                             -- ^ probabilistic program
    -> Comp fs [((a, p), Trace)]                            -- ^ trace of accepted outputs
-metropolis n τ_0 exec prog_0 = do
+metropolis n τ_0 exec model = do
   -- | Perform initial run of mh
-  x0 <- call (exec prog_0 τ_0)
+  x0 <- call (exec model τ_0)
   -- | A function performing n mhSteps using initial mh_s. The most recent samples are at the front of the trace.
-  foldl (>=>) pure (replicate n (metroStep prog_0 exec )) [x0]
+  let metroStep' :: [((a, p), Trace)] -> Comp fs [((a, p), Trace)]
+      metroStep' xs = metroStep model exec (head xs) >>= return . (:xs)
+  foldl (>=>) pure (replicate n metroStep') [x0]
 
 {- | Propose a new sample, execute the model, and then reject or accept the proposal.
 -}
 metroStep :: forall es fs p a. (Members [Proposal p, Sampler] fs)
   => Model es a                                                       -- ^ model handler
   -> ModelHandler es p                                                  -- ^ probabilistic program
-  -> [((a, p), Trace)]                                                   -- ^ previous trace
-  -> Comp fs [((a, p), Trace)]                            -- ^ updated trace
-metroStep prog_0 exec markov_chain = do
-  -- | Get previous iteration output
-  let ((r, p), τ) = head markov_chain
+  -> ((a, p), Trace)                                                   -- ^ previous trace
+  -> Comp fs ((a, p), Trace)                            -- ^ updated trace
+metroStep prog_0 exec ((r, p), τ) = do
   -- | Construct an *initial* proposal
   τ_0            <- call (Propose τ :: Proposal p Trace)
   -- | Execute the model under the initial proposal to return the *final* proposal
   ((r', p'), τ') <- call (exec prog_0 τ_0)
   -- | Compute acceptance ratio
   b              <- call (Accept p p')
-  if b then pure (((r', p'), τ'):markov_chain)
-       else pure markov_chain
+  if b then pure ((r', p'), τ')
+       else pure ((r, p), τ)
 
 {- | Handler for @Sample@ that uses samples from a provided sample trace when possible and otherwise draws new ones.
 -}
